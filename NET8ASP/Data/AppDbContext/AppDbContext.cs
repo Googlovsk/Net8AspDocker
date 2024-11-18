a65e26d859fe5088dd370f47ae9d84e99128cb5a
Googlovsk <lgor2000cch@gmail.com>
2024-11-18 13:25:00 +0300

    Initial commit

---
+++ NET8ASP/Data/AppDbContext/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NET8ASP.Models.Domain;
+
+namespace NET8ASP.Data.AppDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ NET8ASP/Models/Domain/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace NET8ASP.Models.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Car> Cars { get; set; }
+    }
+}
